9d4e3771cffd819973a3ead1c74b7a67937ada1b
Ivan Zheng <IZheng@ucvts.tec.nj.us>
2019-12-13 09:59:36 -0500

    My first commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    document.getElementById("Add").onclick = addOn;
+}
+
+const addOn = function() {
+let addwords = prompt("What would you like to add?");
+if(addwords == null ){
+
+} else {
+    let newadd = document.createElement("li");
+    li.innerHtml = addwords;
+    li.font = "48px sans-serif";
+
+}
+}
