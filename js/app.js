711ca6ae5392721026507cdaef55a362486292a4
Ivan Zheng <IZheng@ucvts.tec.nj.us>
2019-12-13 10:45:09 -0500

    I made the botton and list thingy, just gotta do the array thingy now

--- js/app.js
+++ js/app.js
@@ -1,15 +1,28 @@
+
 window.onload = function() {
     document.getElementById("Add").onclick = addOn;
 }
+const listy = function(){
+    if(listPos == undefined){
+        listPos = 0;
+    } else {
+        listPos++;
+    }
+}
 
 const addOn = function() {
+    let listPos;
+    listy;
+    alert(listPos);
 let addwords = prompt("What would you like to add?");
 if(addwords == null ){
 
 } else {
+    let list = document.getElementById("List");
     let newadd = document.createElement("li");
-    li.innerHtml = addwords;
-    li.font = "48px sans-serif";
+    newadd.innerHTML = addwords;
+    newadd.font = "48px sans-serif";
+    list.append(newadd);
 
 }
 }
